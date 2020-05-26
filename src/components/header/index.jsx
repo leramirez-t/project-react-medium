16e323c569e56bbe61cd3834ddd3e5bcf1d40277
Mar0903 <cristiangm96@hotmail.com>
2020-05-26 14:09:44 -0500

    fixed import items

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import BigCard from './header-big-card/header-big-card'
+import HeaderMiddleList from './header-middle-card-list'
 
 export default function Header (props) {
   const { articles } = props
 
   return (
     <div className="row">
-      <div className="col-12 md-4">
+      <div className="col-4 md-4">
         <BigCard
-          articles={articles[0]}
+          article={articles[0]}
         />
       </div>
-      <div className="col-12 md-4">
+      <div className="col-4 md-4">
         <HeaderMiddleList
           articles={articles.slice(1, 4)}
         />
       </div>
-      <div className="col-12 md-4">
+      <div className="col-4 md-4">
         <BigCard
-          articles={articles[4]}
+          article={articles[4]}
         />
       </div>
     </div>
