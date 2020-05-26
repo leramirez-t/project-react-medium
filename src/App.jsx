bef3058af55e797f45a481086324ef0717b447c0
IPS2792 <ipedrazas@outlook.com>
2020-05-25 21:07:09 -0500

    [UPDATE] App.jsx was updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import {
   Switch,
   Route,
 } from 'react-router-dom'
-import Login from './components/Login'
 
 function App () {
   return (
-    <Login />
-  );
+  )
 }
 
 export default App;
