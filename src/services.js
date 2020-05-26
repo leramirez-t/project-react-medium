35d3185bfcfce2bad4e12951410638d96afa6990
IPS2792 <ipedrazas@outlook.com>
2020-05-25 20:58:15 -0500

    [UPDATE] services.js was updated

--- src/services.js
+++ src/services.js
@@ -1,14 +1,14 @@
-const URL_HOST = "https://localhost:8080/";
+const URL_HOST = 'http://localhost:8080/';
 
 function LogIn(data, callback) {
   const URL_PATH = `${URL_HOST}auth/login`;
   fetch(URL_PATH, {
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(data),
     headers: {
-      "Content-type": "application/json",
+      'Content-type': 'application/json',
     },
-    mode: "cors",
+    mode: 'cors',
   })
     .then(response => response.json())
     .then(data => {
